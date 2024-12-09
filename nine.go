ad952b8b29cb4e867513c83f95e0e22ebf800262
HaleCT2 <halect2@gmail.com>
2024-12-09 17:52:02 -0500

    Day 9 part 2

--- nine.go
+++ nine.go
@@ -16,46 +16,90 @@ func timer(name string) func() {
 
 func main() {
 	defer timer("main")()
-	sb := make([]int, 0)
+	// sb := make([]int, 0)
 
 	input, err := os.ReadFile("nine.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	k := 0
+	intInput := make([]int, 0)
+
+	// k := 0
 	for i := range input {
 		size, err := strconv.Atoi(string(input[i]))
 		if err != nil {
 			panic(err)
 		}
-		if i%2 == 0 || i == 0 {
+		// if i%2 == 0 || i == 0 {
 
-			for j := 0; j < size; j = j + 1 {
-				sb = append(sb, k)
-			}
-			k++
-		} else {
-			for j := 0; j < size; j = j + 1 {
-				sb = append(sb, -1)
+		// 	for j := 0; j < size; j = j + 1 {
+		// 		sb = append(sb, k)
+		// 	}
+		// 	k++
+		// } else {
+		// 	for j := 0; j < size; j = j + 1 {
+		// 		sb = append(sb, -1)
+		// 	}
+		// }
+		intInput = append(intInput, size)
+	}
+
+	place := 0
+	s := 0
+	end := len(intInput) - 1
+	for n := range intInput {
+		if n <= end {
+			if n%2 == 0 || n == 0 {
+				if intInput[n] >= 0 {
+					for i := 0; i < intInput[n]; i++ {
+						s = s + (int(n/2) * place)
+						place++
+					}
+				} else {
+					for j := 0; j > intInput[n]; j-- {
+						place++
+					}
+				}
+			} else {
+				temp := intInput[n]
+				tempEnd := end
+				for temp > 0 && tempEnd > n {
+					if intInput[tempEnd] <= temp && intInput[tempEnd] > 0 {
+						temp = temp - intInput[tempEnd]
+						for i := 0; i < intInput[tempEnd]; i++ {
+							s = s + (int(tempEnd/2) * place)
+							place++
+						}
+						intInput[tempEnd] = -1 * intInput[tempEnd]
+					}
+					tempEnd = tempEnd - 2
+				}
+				if temp > 0 {
+					for j := temp; j > 0; j-- {
+						place++
+					}
+				}
 			}
 		}
 	}
 
-	sum := 0
-	end := len(sb) - 1
-	for b := 0; b < len(sb); b++ {
-		if sb[b] == -1 {
+	// sum := 0
+	// end := len(sb) - 1
+	// for b := 0; b < len(sb); b++ {
+	// 	if sb[b] == -1 {
 
-			for sb[end] == -1 {
-				end = end - 1
-			}
-			sb[b] = sb[end]
-			end = end - 1
-		}
-		if b <= end {
-			sum = sum + (b * sb[b])
-		}
-	}
-	fmt.Println(sum)
+	// 		for sb[end] == -1 {
+	// 			end = end - 1
+	// 		}
+	// 		sb[b] = sb[end]
+	// 		end = end - 1
+	// 	}
+	// 	if b <= end {
+	// 		sum = sum + (b * sb[b])
+	// 	}
+	// }
+
+	fmt.Println(s)
+	// fmt.Println(sum)
 }
